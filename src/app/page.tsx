404ba85e1a19923c3984ff52979b65b0902b2f50
gab-lopez <65933544+gab-lopez@users.noreply.github.com>
2026-06-18 18:58:42 -0600

    fix: remove missing QuoteSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "@/components/sections/HeroSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
 import WhyUsSection from "@/components/sections/WhyUsSection";
-import QuoteSection from "@/components/sections/QuoteSection";
 import ContactSection from "@/components/sections/ContactSection";
 
 export default function Home() {
@@ -12,14 +11,7 @@ export default function Home() {
       <ServicesSection />
       {/* <ProjectsSection /> */}
       <WhyUsSection />
-      <section className="flex flex-col lg:flex-row min-h-screen">
-  <div className="w-full lg:w-1/2">
-    <QuoteSection />
-  </div>
-  <div className="w-full lg:w-1/2">
-    <ContactSection />
-  </div>
-</section>
+      <ContactSection />
     </>
   );
 }
